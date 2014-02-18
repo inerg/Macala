e05e1bca3498a1a0ce7117c658c1f44a3031d98a
inerg <timmer.jo@gmail.com>
2014-02-17 17:00:20 -0700

    Project start

---
+++ src/main.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Author: Inerg
+ * Date: 17/02/14
+ * Time: 4:38 PM
+ */
+public class main {
+    public static void main(String[] args)
+    {
+        System.out.println("hello");
+    }
+}
